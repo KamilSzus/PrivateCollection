c5ce41b1fed97b67e3bbe4eb0a5ac7bcca7810ec
KamilSzus <kamil-szustak1@wp.pl>
2023-11-18 18:14:29 +0100

    Add Db context

---
+++ PrivateCollection/Models/PrivateCollectionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrivateCollection.Models
+{
+    public class PrivateCollectionContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        private string DbPath { get; }
+
+        public PrivateCollectionContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "PrivateCollection.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
