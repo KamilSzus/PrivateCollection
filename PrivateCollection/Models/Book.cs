7385cc7fc9f949351d96e0d833d2caff5ee04ea8
KamilSzus <kamil-szustak1@wp.pl>
2023-11-18 18:14:18 +0100

    Add Book Model

---
+++ PrivateCollection/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace PrivateCollection.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public required string Title { get; set; }
+        public required List<string> Authors { get; set; }
+        public string? Categories { get; set; }
+        public bool IsFinished { get; set; }
+    }
+}
