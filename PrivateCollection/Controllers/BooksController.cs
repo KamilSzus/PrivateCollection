12493265e219ea3f3dc785fe756138b5e4a3827b
KamilSzus <kamil-szustak1@wp.pl>
2023-11-21 22:38:32 +0100

    Add book controller Remove Werher forecast controller and model

---
+++ PrivateCollection/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrivateCollection.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PrivateCollection.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly PrivateCollectionContext _context;
+
+        public BooksController(PrivateCollectionContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var items = await _context.Books.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBook([FromBody]Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                await _context.Books.AddAsync(book);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetBooks", new { book.Id }, book);
+            }
+
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+    }
+    }
