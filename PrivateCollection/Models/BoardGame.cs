1d93d080ad25c7cee05d3dbcc1b8bf54066f3a43
KamilSzus <kamil-szustak1@wp.pl>
2023-11-27 23:15:08 +0100

    Add BoardGame model

---
+++ PrivateCollection/Models/BoardGame.cs
@@ -0,0 +1,16 @@
+﻿namespace PrivateCollection.Models
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int NumberOfGamesPlayed { get; set; } = 0;
+
+        public required string PublishingHouse { get; set; }
+
+        public TimeSpan? InGameTime { get; set; }
+
+        public DateTime? LastGame {  get; set; }
+    }
+}
