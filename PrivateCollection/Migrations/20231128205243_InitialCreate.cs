24f9fbf970d9c741637a357f1f5504422b666581
KamilSzus <kamil-szustak1@wp.pl>
2023-11-28 21:55:48 +0100

    Migration updates

---
+++ PrivateCollection/Migrations/20231128205243_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrivateCollection.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BoardsGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    NumberOfGamesPlayed = table.Column<int>(type: "INTEGER", nullable: false),
+                    PublishingHouse = table.Column<string>(type: "TEXT", nullable: false),
+                    InGameTime = table.Column<TimeSpan>(type: "TEXT", nullable: true),
+                    LastGame = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    GameCount = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoardsGames", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Authors = table.Column<string>(type: "TEXT", nullable: false),
+                    IsFinished = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ReadTime = table.Column<TimeSpan>(type: "TEXT", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Genre = table.Column<int>(type: "INTEGER", nullable: false),
+                    BoardGameId = table.Column<int>(type: "INTEGER", nullable: true),
+                    BookId = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_BoardsGames_BoardGameId",
+                        column: x => x.BoardGameId,
+                        principalTable: "BoardsGames",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Categories_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_BoardGameId",
+                table: "Categories",
+                column: "BoardGameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_BookId",
+                table: "Categories",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "BoardsGames");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
