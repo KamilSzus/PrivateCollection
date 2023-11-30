d3bff0f743f9e9f060f3ca1656fcdfc74d8958f0
KamilSzus <kamil-szustak1@wp.pl>
2023-11-30 21:29:42 +0100

    Add implement method from IBookRepository

--- PrivateCollection/Interfaces/IBookRepository.cs
+++ PrivateCollection/Interfaces/IBookRepository.cs
@@ -5,5 +5,9 @@ namespace PrivateCollection.Interfaces
     public interface IBookRepository
     {
         public Task<ICollection<Book>> GetBooks();
+        public Task<Book?> GetBookById(int id);
+        public Task<Book?> GetBookByTitle(string title);
+        public Task<bool> BookExist(int id);
+        public Task<ICollection<Book>> GetUnFishedBooks();
     }
 }
--- PrivateCollection/Repository/BookRepository.cs
+++ PrivateCollection/Repository/BookRepository.cs
@@ -14,9 +14,29 @@ namespace PrivateCollection.Repository
             this.Context = context;
         }
 
+        public async Task<bool> BookExist(int id)
+        {
+            return await this.Context.Books.AnyAsync(b => b.Id == id);
+        }
+
+        public async Task<Book?> GetBookById(int id)
+        {
+            return await this.Context.Books.Where(b => b.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Book?> GetBookByTitle(string title)
+        {
+            return await this.Context.Books.Where(b => b.Title == title).FirstOrDefaultAsync();
+        }
+
         public async Task<ICollection<Book>> GetBooks()
         {
             return await this.Context.Books.OrderBy(b => b.Title).ToListAsync();
+        }
+
+        public async Task<ICollection<Book>> GetUnFishedBooks()
+        {
+            return await this.Context.Books.Where(b => b.IsFinished == false).ToListAsync();
         }
     }
 }
